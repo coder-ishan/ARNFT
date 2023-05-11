dc10c324f8d9ee2372fe12299313a4c06cc6955a
Ishan Singh <Is26092003@gmail.com>
2023-05-12 01:29:40 +0530

    AllMOSTDONE

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import FileUpload from './FileUpload'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="App">
+    <h1>Mint NFT</h1>
+    <div>AR FILTER NFT</div>
+    <FileUpload></FileUpload>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
