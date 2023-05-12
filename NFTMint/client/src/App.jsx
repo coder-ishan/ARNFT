b5e5f8998a68cc21b756d72255d80ee9f6d270ec
Ishan Singh <Is26092003@gmail.com>
2023-05-12 10:18:51 +0530

    completed

---
+++ NFTMint/client/src/App.jsx
@@ -0,0 +1,20 @@
+import FileUpload from './FileUpload'
+import './App.css'
+function App() {
+  
+  return (
+    <>
+    <div className="App">
+    <h1>Mint NFT</h1>
+    <div>
+      <img src="/currency.png" alt="" width ={100} height={100}/>
+    </div>
+    <div>AR FILTER NFT</div>
+    <FileUpload></FileUpload>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
